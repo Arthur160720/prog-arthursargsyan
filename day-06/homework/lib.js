8b588d74bb8326548a82924fff75c7739b55105e
artur <arthur.sargsyan.00@mail.ru>
2022-08-16 10:38:36 +0400

    change files

--- day-06/homework/lib.js
+++ day-06/homework/lib.js
@@ -2,6 +2,16 @@ function isEven(number) {
     return number % 2 === 0;
 }
 
+function areaOfCube(side) {
+    return side ** 3;
+}
+
+function lateralSurface(side) {
+    return 6 * side * side;
+}
+
 module.exports = {
     isEven,
+    areaOfCube,
+    lateralSurface
 }
